801b7103883e21dfc6ca9026daf8323041b3af4c
Jim <jimovermeen@hotmail.nl>
2024-04-06 22:13:50 +0200

    delete unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import AddNewLoginScreen from "./components/AddNewLoginScreen/AddNewLoginScreen";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import ShowLoginDetailsScreen from "./components/ShowLoginDetailsScreen/ShowLoginDetailsScreen";
